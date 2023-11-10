861db4e38f0dabe9133a2b1dd5b37b07f36b2985
AnnVynnychenkoa <avinnichenko93@gmail.com>
2023-11-10 23:44:19 +0200

    prof-set-correct

--- src/components/App/SharedLayout.jsx
+++ src/components/App/SharedLayout.jsx
@@ -1,13 +1,11 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
-import User from '../../pages/User';
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
-      <User />
       <main>
         <Suspense fallback={null}>
           <Outlet />
@@ -18,8 +16,3 @@ const SharedLayout = () => {
 };
 
 export default SharedLayout;
-
-
-blood - number; allowed values 1, 2, 3, 4; required
-sex - string; allowed values "male", "female"; required
-levelActivity - number; allowed values 1, 2, 3, 4, 5; required
