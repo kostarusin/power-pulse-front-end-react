1b5f3875004e544e2906c6e2fbc1252f7fab895f
Artem <117488314+ArtemBortyaniy@users.noreply.github.com>
2023-11-11 23:05:53 +0200

    added work with redux auth and added user login and update functionality

--- src/components/SignUpInForm/SignUpInForm.jsx
+++ src/components/SignUpInForm/SignUpInForm.jsx
@@ -1,25 +1,39 @@
+import { useDispatch } from 'react-redux';
+import { register } from '../../redux/auth/operations';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import css from './SignUpInForm.module.css';
 
 const SignUpInForm = () => {
+  const dispatch = useDispatch();
+
   const initialValues = {
-    name: '',
+    username: '',
     email: '',
     password: '',
   };
 
-  //   const SignUpSchema = Yup.object().shape({
-  //     name: Yup.string().required(),
-  //     email: Yup.string().email('Invalid email').required('Required'),
-  //     password: Yup.string().email('Invalid password').required('Required'),
-  //   });
+  const SignUpSchema = Yup.object().shape({
+    username: Yup.string().required('Name is required'),
+    email: Yup.string()
+      .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Invalid email format')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Password is required'),
+  });
+
+  function handleSubmit(values, { resetForm }) {
+    dispatch(register(values));
+
+    resetForm();
+  }
 
   return (
     <Formik
       initialValues={initialValues}
-      //   onSubmit={handleSubmit}
-      //   validationSchema={SignUpSchema}
+      onSubmit={handleSubmit}
+      validationSchema={SignUpSchema}
     >
       <Form className={css.form}>
         <label htmlFor="username">
