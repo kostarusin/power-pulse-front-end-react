7b112e56a23ab641936cc9616e875de7fcdb0004
Viktoria <viktoria.ttravel@gmail.com>
2023-11-11 14:45:45 +0200

    first page PC (3)

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const SignUp = () => {
-  return (
-    <div>
-      SignUp
-    </div>
-  )
-}
+  return <div>SignUp</div>;
+};
 
-export default SignUp
+export default SignUp;
