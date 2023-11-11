32a4521b05b40663323669c8c33ea1399c438aea
Viktoria <viktoria.ttravel@gmail.com>
2023-11-11 20:24:32 +0200

    SignUp PC

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React from 'react';
+import SignUpInForm from '../../components/SignUpInForm/SignUpInForm';
+import css from './SignUp.module.css';
 const SignUp = () => {
   return (
-    <div>
-      SignUp
-    </div>
-  )
-}
+    <>
+      <div className={css.container}>
+        <h2 className={css.title}>SignUp</h2>
+        <p className={css.text}>
+          Thank you for your interest in our platform. To complete the
+          registration process, please provide us with the following
+          information.
+        </p>
+        <SignUpInForm />
+      </div>
+    </>
+  );
+};
 
-export default SignUp
+export default SignUp;
