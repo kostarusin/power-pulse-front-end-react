e98e6eb7c90b64f5beb8243fc8e337fc9a3d231b
kostarusin <122443715+kostarusin@users.noreply.github.com>
2023-11-13 06:31:30 +0000

    Deploying to gh-pages from @ kostarusin/power-pulse-front-end-react@213f0537689c33858125804c60be0b08a1b647b0 🚀

--- assets/Error-c99a7fd0.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-58ce9434.js";import{H as r}from"./HomeBackground-8fd8c2da.js";const t=()=>s.jsxs("section",{className:css.error,children:[s.jsx("h1",{children:"404"}),s.jsx(r,{})]});export{t as default};
