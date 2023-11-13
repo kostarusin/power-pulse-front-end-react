f647425fe73cacce1cc9afa9dd60aae3a263934a
ArtemBortyaniy <117488314+ArtemBortyaniy@users.noreply.github.com>
2023-11-13 23:26:49 +0000

    Deploying to gh-pages from @ kostarusin/power-pulse-front-end-react@9bf3afe598ee212acc15758e3ae9ef2317251e06 🚀

---
+++ assets/icons-ab5d985a.js
@@ -0,0 +1,1 @@
+import"./index-d0a0f542.js";const o="/power-pulse-front-end-react/assets/icons-bc282080.svg";export{o as i};
--- assets/icons-f1a7365c.js
+++
@@ -1,1 +0,0 @@
-import"./index-da8e9fa3.js";const o="/power-pulse-front-end-react/assets/icons-bc282080.svg";export{o as i};
