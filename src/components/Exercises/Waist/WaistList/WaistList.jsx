9e63a61fb329f3a0672ce733ca3c78b42336db79
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-11-13 15:48:29 +0200

    update

---
+++ src/components/Exercises/Waist/WaistList/WaistList.jsx
@@ -0,0 +1,88 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { WaistItem } from '../WaistItem/WaistItem';
+import {
+  WaistItemUl,
+  NoExercisesTitle,
+  WaistListContainer,
+  ImgWaist,
+} from './WaistList.styled';
+import { selectExercises } from '../../../../redux/exercises/selectorsExercises';
+import { useEffect } from 'react';
+import { fetchExercises } from '../../../../redux/exercises/operationsExercises';
+import images from '../../../../images/waist-1x.jpg';
+import { useState } from 'react';
+import BasicModalWindow from '../../../BasicModalWindow/BasicModalWindow';
+import { ExersiceModalWindow } from '../../../ExersiceModalWindow/ExersiceModalWindow';
+import { SuccessExerciseModalWindow } from '../../../ExersiceModalWindow/SuccessExerciseModalWindow/SuccessExerciseModalWindow';
+
+export const WaistList = ({ exerciseName }) => {
+  const [modalChange, setModalChange] = useState('');
+  const [modalData, setModalData] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchExercises());
+  }, [dispatch]);
+
+  const openModalToggle = el => {
+    setModalData(el);
+  };
+
+  const closeModal = () => {
+    setModalData(null);
+  };
+
+  const changeModal = () => {
+    setModalChange('well');
+  };
+
+  const exercises = useSelector(selectExercises);
+  const visibleExercises = exercises.filter(
+    exercise =>
+      exercise.bodyPart ||
+      exercise.target ||
+      exercise.equipment === exerciseName,
+  );
+  return (
+    <>
+      {modalData && (
+        <BasicModalWindow isOpenModalToggle={closeModal}>
+          {modalChange !== 'well' ? (
+            <ExersiceModalWindow
+              data={modalData}
+              closeModal={closeModal}
+              onClick={changeModal}
+            />
+          ) : (
+            <SuccessExerciseModalWindow
+              closeModal={closeModal}
+              data={modalData}
+              onClick={closeModal}
+            />
+          )}
+        </BasicModalWindow>
+      )}
+      <WaistListContainer>
+        <WaistItemUl>
+          {visibleExercises.length ? (
+            visibleExercises
+              .slice(0, 50)
+              .map(el => (
+                <WaistItem
+                  key={el._id}
+                  data={el}
+                  openModalToggle={openModalToggle}
+                />
+              ))
+          ) : (
+            <NoExercisesTitle>
+              There is not exercises downloaded else, plaese try choose this
+              categorie later!!!
+            </NoExercisesTitle>
+          )}
+        </WaistItemUl>
+        <ImgWaist src={images} alt="image" />
+      </WaistListContainer>
+    </>
+  );
+};
--- src/redux/exercises/operationsExercises.js
+++ src/redux/exercises/operationsExercises.js
@@ -1,11 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+axios.defaults.baseURL = 'https://power-pulse-back-end.onrender.com';
+
+
 
 export const fetchExercises = createAsyncThunk(
   'exercises/fetchExercises',
   async (_, thunkAPI) => {
     try {
-      const res = await axios.get('/exercises');
+      const res = await axios.get('api/exercises');
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -17,7 +20,7 @@ export const fetchBodyParts = createAsyncThunk(
   'exercises/fetchBodyParts',
   async (_, thunkAPI) => {
     try {
-      const res = await axios.get('/exercises/bodyparts');
+      const res = await axios.get('api/exercises/bodyparts');
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
