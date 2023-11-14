eca7f96750fb97a8d2ee5ca92d823b788451e65d
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2023-11-15 00:37:31 +0100

    open-close-modal

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -4,31 +4,33 @@ import { useDispatch } from 'react-redux';
 import { logOut } from '../../redux/auth/operations';
 import sprite from '../../assets/icons-optimized.svg';
 
-const MobileMenu = ({ onClose }) => {
+const MobileMenu = ({ isOpen, onClose }) => {
   const dispatch = useDispatch();
   return (
-    <div className={css.mobileMenuWrap}>
-      <div className={css.xButton} onClick={onClose}>
-        &times;
-      </div>
-      <div className={css.userNaviWrap}>
-        <Link className={css.mobMenuItem} to="/diary">
-          Diary
-        </Link>
-        <Link className={css.mobMenuItem} to="/exercises">
-          Exercises
-        </Link>
-        <Link className={css.mobMenuItem} to="/products">
-          Products
-        </Link>
-      </div>
-      <div className={css.loguotWrap}>
-        <Link className={css.logoutLink} onClick={() => dispatch(logOut())}>
-          <p className={css.logoutText}>Logout</p>
-          <svg className={css.logoutIcon}>
-            <use href={`${sprite}#icon-log-out-white`} />
-          </svg>
-        </Link>
+    <div className={`${css.backdrop} ${isOpen ? css.backdropVisible : ''}`}>
+      <div className={css.mobileMenuWrap}>
+        <div className={css.xButton} onClick={onClose}>
+          &times;
+        </div>
+        <div className={css.userNaviWrap}>
+          <Link className={css.mobMenuItem} onClick={onClose} to="/diary">
+            Diary
+          </Link>
+          <Link className={css.mobMenuItem} onClick={onClose} to="/exercises">
+            Exercises
+          </Link>
+          <Link className={css.mobMenuItem} onClick={onClose} to="/products">
+            Products
+          </Link>
+        </div>
+        <div className={css.logoutWrap} onClick={onClose}>
+          <Link className={css.logoutLink} onClick={() => dispatch(logOut())}>
+            <p className={css.logoutText}>Logout</p>
+            <svg className={css.logoutIcon}>
+              <use href={`${sprite}#icon-log-out-white`} />
+            </svg>
+          </Link>
+        </div>
       </div>
     </div>
   );
