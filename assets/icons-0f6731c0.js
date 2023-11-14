a5310b51c3104763170d384ded11f92ceea44062
Liliya-ivanovna <123566371+Liliya-ivanovna@users.noreply.github.com>
2023-11-14 18:56:18 +0000

    Deploying to gh-pages from @ kostarusin/power-pulse-front-end-react@876eab763f7b63a6f73c5214511de794eb06d554 🚀

--- assets/icons-0f6731c0.js
+++
@@ -1,1 +0,0 @@
-import"./index-187677b2.js";const o="/power-pulse-front-end-react/assets/icons-bc282080.svg";export{o as i};
---
+++ assets/icons-f63ecc5f.js
@@ -0,0 +1,1 @@
+import"./index-8ba16fe3.js";const o="/power-pulse-front-end-react/assets/icons-bc282080.svg";export{o as i};
