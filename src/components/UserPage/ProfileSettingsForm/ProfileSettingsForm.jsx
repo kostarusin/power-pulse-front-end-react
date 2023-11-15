67482369be8469bd546fbf3b3152069e3c058e8a
AnnVynnychenkoa <avinnichenko93@gmail.com>
2023-11-15 18:51:09 +0200

    save-disabled

--- src/components/UserPage/ProfileSettingsForm/ProfileSettingsForm.jsx
+++ src/components/UserPage/ProfileSettingsForm/ProfileSettingsForm.jsx
@@ -1,6 +1,7 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import ProfileSettingsSchema from './yapValidateSchema';
 import isEqual from 'lodash/isEqual';
+import _ from 'lodash';
 //redux
 import { useDispatch } from 'react-redux';
 import { updateInfo } from '../../../redux/auth/operations';
@@ -9,69 +10,60 @@ import { useAuth } from '../../../redux/hooks';
 
 //styles
 import css from './ProfileSettingsForm.module.css';
-import { useEffect, useMemo, useState } from 'react';
+import { useState } from 'react';
 
 const ProfileSettingsForm = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
 
-  // const isFormDataMatchingBackend = useMemo(() => {
-  //   return (
-  //     user.username === values.username &&
-  //     user.email === values.email &&
-  //     user.height === values.height &&
-  //     user.currentWeight === values.currentWeight &&
-  //     user.desiredWeight === values.desiredWeight &&
-  //     user.birthday === values.birthday &&
-  //     user.blood === values.blood &&
-  //     user.sex === values.sex &&
-  //     user.levelActivity === values.levelActivity
-  //   );
-  // }, [user]);
-  // useEffect(() => {
-  //   const isFormDataChanged = Object.keys(initialValues).some(
-  //     (key) => initialValues[key] !== user[key],
-  //   );
-  //   setIsFormChanged(isFormDataChanged);
-  // }, [user]);
+  const [isFormChanged, setFormChanged] = useState(false);
 
-  const handleSubmit = (values, { resetForm }) => {
-    const areEqual = isEqual(values, user);
-    if (areEqual) {
-      console.log('The objects have the same values.');
-    } else {
-      console.log('The objects have different values.');
-    }
-    // const updatedValues = { ...values };
-    // delete updatedValues.email;
-    // dispatch(updateInfo(updatedValues));
-    // resetForm();
+  const convertValues = (obj) => {
+    return _.omitBy(
+      _.mapValues(obj, (value, key) => {
+        if (key === 'birthday') {
+          return value.split('T')[0];
+        } else if (_.isNumber(value) || _.isString(value)) {
+          return _.isNaN(Number(value)) ? value : Number(value);
+        }
+        return value;
+      }),
+      (_, key) => key === 'avatarURL',
+    );
   };
 
-  const [isFormChanged, setIsFormChanged] = useState(false);
+  const handleSubmit = (values, { resetForm }) => {
+    const convertedUser = convertValues(user);
+    const convertedValues = convertValues(values);
+    const areEqual = isEqual(convertedUser, convertedValues);
 
-  const handleInputChange = (formik, event) => {
-    formik.handleChange(event);
+    if (areEqual) {
+      console.log('The objects have the same values.');
+      setFormChanged(false);
+    } else {
+      const updatedValues = { ...values };
+      delete updatedValues.email;
+      dispatch(updateInfo(updatedValues));
+      setFormChanged(true);
+    }
 
-    const currentFormValues = formik.values;
-    const isFormDataChanged = user.height === currentFormValues.height;
-    console.log('currentFormValues.height', currentFormValues.height);
-    // console.log('user.height', user.height);
-    // console.log('isFormDataChanged', isFormDataChanged);
+    resetForm();
+  };
 
-    setIsFormChanged(isFormDataChanged);
+  const handleFormChange = () => {
+    setFormChanged(true);
   };
 
   const initialValues = {
     username: user.username,
     email: user.email,
-    height: '',
-    currentWeight: '',
-    desiredWeight: '',
-    birthday: '',
-    blood: '',
-    sex: '',
-    levelActivity: '',
+    height: user.height,
+    currentWeight: user.currentWeight,
+    desiredWeight: user.desiredWeight,
+    birthday: user.birthday,
+    blood: user.blood,
+    sex: user.sex,
+    levelActivity: user.levelActivity,
   };
 
   return (
@@ -80,246 +72,223 @@ const ProfileSettingsForm = () => {
       onSubmit={handleSubmit}
       validationSchema={ProfileSettingsSchema}
     >
-      {(formik) => (
-        <Form className={css.formContainer}>
-          <div className={css.basicInfoContainer}>
-            <label htmlFor="username" className={css.fieldWithError}>
-              <div className={css.setName}>Basic info</div>
+      <Form className={css.formContainer} onChange={handleFormChange}>
+        <div className={css.basicInfoContainer}>
+          <label htmlFor="username" className={css.fieldWithError}>
+            <div className={css.setName}>Basic info</div>
+            <Field
+              className={css.basicInfoInput}
+              name="username"
+              type="text"
+              placeholder="Anna Rybachok"
+            />
+            <ErrorMessage
+              component="div"
+              name="username"
+              className={css.ErrorMessage}
+            />
+          </label>
+          <label htmlFor="email" className={css.fieldWithError}>
+            <Field
+              className={css.basicInfoInput}
+              name="email"
+              type="email"
+              placeholder="annarybachok@gmail.com"
+              readOnly
+            />
+            <ErrorMessage
+              component="div"
+              name="email"
+              className={css.ErrorMessage}
+            />
+          </label>
+        </div>
+        <div className={css.secondaryInfoContainer}>
+          <div className={css.flexContainer}>
+            <label htmlFor="height" className={css.secondaryInfoLabel}>
+              <span className={css.setName}>Height</span>
               <Field
-                className={css.basicInfoInput}
-                name="username"
-                type="text"
-                onChange={(event) => handleInputChange(formik, event)}
-                placeholder="Anna Rybachok"
+                className={css.infoInput}
+                name="height"
+                type="number"
+                placeholder="0"
               />
               <ErrorMessage
                 component="div"
-                name="username"
+                name="height"
                 className={css.ErrorMessage}
               />
             </label>
-            <label htmlFor="email" className={css.fieldWithError}>
+            <label htmlFor="currentWeight" className={css.secondaryInfoLabel}>
+              <span className={css.setName}>Current Weight</span>
               <Field
-                className={css.basicInfoInput}
-                name="email"
-                type="email"
-                placeholder="annarybachok@gmail.com"
-                readOnly
+                className={css.infoInput}
+                name="currentWeight"
+                type="number"
+                placeholder="0"
               />
               <ErrorMessage
                 component="div"
-                name="email"
+                name="currentWeight"
                 className={css.ErrorMessage}
               />
             </label>
           </div>
-          <div className={css.secondaryInfoContainer}>
-            <div className={css.flexContainer}>
-              <label htmlFor="height" className={css.secondaryInfoLabel}>
-                <span className={css.setName}>Height</span>
-                <Field
-                  className={css.infoInput}
-                  name="height"
-                  type="number"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  placeholder="0"
-                />
-                <ErrorMessage
-                  component="div"
-                  name="height"
-                  className={css.ErrorMessage}
-                />
-              </label>
-              <label htmlFor="currentWeight" className={css.secondaryInfoLabel}>
-                <span className={css.setName}>Current Weight</span>
-                <Field
-                  className={css.infoInput}
-                  name="currentWeight"
-                  type="number"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  placeholder="0"
-                />
-                <ErrorMessage
-                  component="div"
-                  name="currentWeight"
-                  className={css.ErrorMessage}
-                />
-              </label>
-            </div>
-            <div className={css.flexContainer}>
-              <label htmlFor="desiredWeight" className={css.secondaryInfoLabel}>
-                <span className={css.setName}>Desired Weight</span>
-                <Field
-                  className={css.infoInput}
-                  name="desiredWeight"
-                  type="number"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  placeholder="0"
-                />
-                <ErrorMessage
-                  component="div"
-                  name="desiredWeight"
-                  className={css.ErrorMessage}
-                />
-              </label>
-              <label htmlFor="birthday">
-                <Field
-                  className={css.infoInput}
-                  name="birthday"
-                  type="date"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  placeholder="00.00.0000"
-                />
-                <ErrorMessage
-                  component="div"
-                  name="birthday"
-                  className={css.ErrorMessage}
-                />
-              </label>
-            </div>
+          <div className={css.flexContainer}>
+            <label htmlFor="desiredWeight" className={css.secondaryInfoLabel}>
+              <span className={css.setName}>Desired Weight</span>
+              <Field
+                className={css.infoInput}
+                name="desiredWeight"
+                type="number"
+                placeholder="0"
+              />
+              <ErrorMessage
+                component="div"
+                name="desiredWeight"
+                className={css.ErrorMessage}
+              />
+            </label>
+            <label htmlFor="birthday">
+              <Field
+                className={css.infoInput}
+                name="birthday"
+                type="date"
+                placeholder="00.00.0000"
+              />
+              <ErrorMessage
+                component="div"
+                name="birthday"
+                className={css.ErrorMessage}
+              />
+            </label>
           </div>
-          <div id="blood-radio-group" className={css.setName}>
-            Blood
-          </div>
-          <div className={css.bloodSexGroup}>
-            <div
-              role="group"
-              aria-labelledby="blood-radio-group"
-              className={css.radioGroup}
-            >
-              <label className={css.radioBtnLabel}>
-                <Field
-                  className={css.radioBtn}
-                  type="radio"
-                  name="blood"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  value="1"
-                />
-                <span className={css.customRadioBtn}></span>1
-              </label>
-              <label className={css.radioBtnLabel}>
-                <Field
-                  className={css.radioBtn}
-                  type="radio"
-                  name="blood"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  value="2"
-                />
-                <span className={css.customRadioBtn}></span>2
-              </label>
-              <label className={css.radioBtnLabel}>
-                <Field
-                  className={css.radioBtn}
-                  type="radio"
-                  name="blood"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  value="3"
-                />
-                <span className={css.customRadioBtn}></span>3
-              </label>
-              <label className={css.radioBtnLabel}>
-                <Field
-                  className={css.radioBtn}
-                  type="radio"
-                  name="blood"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  value="4"
-                />
-                <span className={css.customRadioBtn}></span>4
-              </label>
-            </div>
-            <div role="group" className={css.radioGroup}>
-              <label className={css.radioBtnLabel}>
-                <Field
-                  className={css.radioBtn}
-                  type="radio"
-                  name="sex"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  value="male"
-                />
-                <span className={css.customRadioBtn}></span>
-                Male
-              </label>
-              <label className={css.radioBtnLabel}>
-                <Field
-                  className={css.radioBtn}
-                  type="radio"
-                  name="sex"
-                  onChange={(event) => handleInputChange(formik, event)}
-                  value="female"
-                />
-                <span className={css.customRadioBtn}></span>
-                Female
-              </label>
-            </div>
-          </div>
-          <div role="group" className={css.lifestyleGroup}>
+        </div>
+        <div id="blood-radio-group" className={css.setName}>
+          Blood
+        </div>
+        <div className={css.bloodSexGroup}>
+          <div
+            role="group"
+            aria-labelledby="blood-radio-group"
+            className={css.radioGroup}
+          >
             <label className={css.radioBtnLabel}>
               <Field
                 className={css.radioBtn}
                 type="radio"
-                name="levelActivity"
-                onChange={(event) => handleInputChange(formik, event)}
+                name="blood"
                 value="1"
               />
-              <span className={css.customRadioBtn}></span>
-              Sedentary lifestyle (little or no physical activity)
+              <span className={css.customRadioBtn}></span>1
             </label>
             <label className={css.radioBtnLabel}>
               <Field
                 className={css.radioBtn}
                 type="radio"
-                name="levelActivity"
-                onChange={(event) => handleInputChange(formik, event)}
+                name="blood"
                 value="2"
               />
-              <span className={css.customRadioBtn}></span>
-              Light activity (light exercises/sports 1-3 days per week)
+              <span className={css.customRadioBtn}></span>2
             </label>
             <label className={css.radioBtnLabel}>
               <Field
                 className={css.radioBtn}
                 type="radio"
-                name="levelActivity"
-                onChange={(event) => handleInputChange(formik, event)}
+                name="blood"
                 value="3"
               />
-              <span className={css.customRadioBtn}></span>
-              Moderately active (moderate exercises/sports 3-5 days per week)
+              <span className={css.customRadioBtn}></span>3
             </label>
             <label className={css.radioBtnLabel}>
               <Field
                 className={css.radioBtn}
                 type="radio"
-                name="levelActivity"
-                onChange={(event) => handleInputChange(formik, event)}
+                name="blood"
                 value="4"
               />
+              <span className={css.customRadioBtn}></span>4
+            </label>
+          </div>
+          <div role="group" className={css.radioGroup}>
+            <label className={css.radioBtnLabel}>
+              <Field
+                className={css.radioBtn}
+                type="radio"
+                name="sex"
+                value="male"
+              />
               <span className={css.customRadioBtn}></span>
-              Very active (intense exercises/sports 6-7 days per week)
+              Male
             </label>
             <label className={css.radioBtnLabel}>
               <Field
                 className={css.radioBtn}
                 type="radio"
-                name="levelActivity"
-                onChange={(event) => handleInputChange(formik, event)}
-                value="5"
+                name="sex"
+                value="female"
               />
               <span className={css.customRadioBtn}></span>
-              Extremely active (very strenuous exercises/sports and physical
-              work)
+              Female
             </label>
           </div>
-          <button
-            className={css.saveBtn}
-            type="submit"
-            disabled={isFormChanged}
-          >
-            Save
-          </button>
-        </Form>
-      )}
+        </div>
+        <div role="group" className={css.lifestyleGroup}>
+          <label className={css.radioBtnLabel}>
+            <Field
+              className={css.radioBtn}
+              type="radio"
+              name="levelActivity"
+              value="1"
+            />
+            <span className={css.customRadioBtn}></span>
+            Sedentary lifestyle (little or no physical activity)
+          </label>
+          <label className={css.radioBtnLabel}>
+            <Field
+              className={css.radioBtn}
+              type="radio"
+              name="levelActivity"
+              value="2"
+            />
+            <span className={css.customRadioBtn}></span>
+            Light activity (light exercises/sports 1-3 days per week)
+          </label>
+          <label className={css.radioBtnLabel}>
+            <Field
+              className={css.radioBtn}
+              type="radio"
+              name="levelActivity"
+              value="3"
+            />
+            <span className={css.customRadioBtn}></span>
+            Moderately active (moderate exercises/sports 3-5 days per week)
+          </label>
+          <label className={css.radioBtnLabel}>
+            <Field
+              className={css.radioBtn}
+              type="radio"
+              name="levelActivity"
+              value="4"
+            />
+            <span className={css.customRadioBtn}></span>
+            Very active (intense exercises/sports 6-7 days per week)
+          </label>
+          <label className={css.radioBtnLabel}>
+            <Field
+              className={css.radioBtn}
+              type="radio"
+              name="levelActivity"
+              value="5"
+            />
+            <span className={css.customRadioBtn}></span>
+            Extremely active (very strenuous exercises/sports and physical work)
+          </label>
+        </div>
+        <button className={css.saveBtn} type="submit" disabled={!isFormChanged}>
+          Save
+        </button>
+      </Form>
     </Formik>
   );
 };
