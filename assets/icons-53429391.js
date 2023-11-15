18984e4d390078f36e59c8f98b74104bf4b51e67
kostarusin <122443715+kostarusin@users.noreply.github.com>
2023-11-15 11:17:27 +0000

    Deploying to gh-pages from @ kostarusin/power-pulse-front-end-react@909e25044337654cfe5a7e36906c12156dab0397 🚀

--- assets/icons-53429391.js
+++
@@ -1,1 +0,0 @@
-import"./index-2de18e7c.js";const o="/power-pulse-front-end-react/assets/icons-bc282080.svg";export{o as i};
---
+++ assets/icons-c5165a36.js
@@ -0,0 +1,1 @@
+import"./index-f1731e12.js";const o="/power-pulse-front-end-react/assets/icons-bc282080.svg";export{o as i};
