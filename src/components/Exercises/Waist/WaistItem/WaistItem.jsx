bc2e8e815637baafb24a90629d1f32a2676fe5f6
Artem <117488314+ArtemBortyaniy@users.noreply.github.com>
2023-11-16 16:55:18 +0200

    added work modal in exercises

--- src/components/Exercises/Waist/WaistItem/WaistItem.jsx
+++ src/components/Exercises/Waist/WaistItem/WaistItem.jsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+//components
+import { Modal } from '../../../Modal/Modal';
+import { AddExerciseForm } from '../../../AddExerciseForm/AddExerciseForm';
+import { AddExerciseSuccess } from '../../../AddExerciseSuccess/AddExerciseSuccess';
+//stele
 import css from './WaistItem.module.css';
 import icons from '../../../../assets/icons.svg';
 
@@ -11,19 +17,32 @@ const texts = {
   },
 };
 
-const capitalizeFirstLeter = string => {
-  const newString = string.slice(0, 1).toUpperCase() + string.slice(1);
-  return newString;
-};
+export const WaistItem = ({ data }) => {
+  const [showExerciseModal, setShowExerciseModal] = useState(false);
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const [exercise, setExercise] = useState({});
+  console.log(data);
+  const capitalizeFirstLeter = (string) => {
+    const newString = string.slice(0, 1).toUpperCase() + string.slice(1);
+    return newString;
+  };
 
-export const WaistItem = ({ data,toggleExerciseModal }) => {
-  
+  const toggleExerciseModal = () => {
+    setShowExerciseModal((prevState) => !prevState);
+  };
+
+  const toggleSuccessModal = () => {
+    setShowSuccessModal((prevState) => !prevState);
+  };
+
   return (
     <li className={css.waistItemLi}>
       <div className={css.btnWrapper}>
         <p className={css.cardLabel}>{texts.cardLabel}</p>
-        <button className={css.btnLabel}
-         type="button"  onClick={() => {toggleExerciseModal(data)}}
+        <button
+          className={css.btnLabel}
+          type="button"
+          onClick={toggleExerciseModal}
         >
           {texts.btnLabel}
           <span>
@@ -37,15 +56,32 @@ export const WaistItem = ({ data,toggleExerciseModal }) => {
         <span className={css.spanExerciseRun}>
           <svg className={css.svgExerciseRun} width={24} height={24}>
             <use href={`${icons}#icon-runn   ing-figure`}></use>
-            <use href={`${icons}#icon-running-stick-figure-svgrepo-com-1`}></use>
+            <use
+              href={`${icons}#icon-running-stick-figure-svgrepo-com-1`}
+            ></use>
           </svg>
         </span>
         <h3 className={css.title}>{capitalizeFirstLeter(data.name)} </h3>
       </div>
+      {showExerciseModal && (
+        <Modal onClose={toggleExerciseModal}>
+          <AddExerciseForm
+            exercise={data}
+            toggleExerciseModal={toggleExerciseModal}
+            toggleSuccessModal={toggleSuccessModal}
+            setExercise={setExercise}
+          />
+        </Modal>
+      )}
+      {showSuccessModal && (
+        <Modal onClose={toggleSuccessModal}>
+          <AddExerciseSuccess exercise={exercise} />
+        </Modal>
+      )}
 
       <ul className={css.list}>
         {Object.keys(texts.list).map((e) => (
-          <li className={css.listItem}key={e}>
+          <li className={css.listItem} key={e}>
             {texts.list[e]}
             <span className={css.listItemValue}>
               {capitalizeFirstLeter(String(data[e]))}
