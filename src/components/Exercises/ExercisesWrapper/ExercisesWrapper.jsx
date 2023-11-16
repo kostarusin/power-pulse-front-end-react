60d0c0ef9ab2d4d67af5f281553cbb3f974aa4d0
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-11-16 20:48:04 +0200

    Update ExercisesWrapper.jsx

--- src/components/Exercises/ExercisesWrapper/ExercisesWrapper.jsx
+++ src/components/Exercises/ExercisesWrapper/ExercisesWrapper.jsx
@@ -7,7 +7,7 @@ import { ExercisesNavigation } from '../ExercisesNavigation/ExercisesNavigation'
 import { WaistList } from '../Waist/WaistList/WaistList';
 
 export const ExercisesWrap = () => {
-  const [activeFilter, setActiveFilter] = useState('Body parts');
+  const [activeFilter, setActiveFilter] = useState('Body part');
   const [exerciseName, setExerciseName] = useState('');
 
   const handleSetExName = (name) => {
