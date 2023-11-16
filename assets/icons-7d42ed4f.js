8a13e7bbbec6d8a70479ddbacc4e83f671c6064c
Liliya-ivanovna <123566371+Liliya-ivanovna@users.noreply.github.com>
2023-11-16 18:59:26 +0000

    Deploying to gh-pages from @ kostarusin/power-pulse-front-end-react@73319b1df21094a3cbc90ffae3f82535eefe1102 🚀

--- assets/icons-7d42ed4f.js
+++
@@ -1,1 +0,0 @@
-import"./index-3188d71c.js";const o="/power-pulse-front-end-react/assets/icons-6688b68f.svg";export{o as i};
---
+++ assets/icons-e94a159d.js
@@ -0,0 +1,1 @@
+import"./index-5db3a14a.js";const o="/power-pulse-front-end-react/assets/icons-6688b68f.svg";export{o as i};
