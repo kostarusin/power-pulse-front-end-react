c0df930d7b859b6dee5c7617ff65a1808a58f73e
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-11-17 02:42:43 +0200

    fix-css

--- src/components/diary/TitlePage/TitlePage.jsx
+++ src/components/diary/TitlePage/TitlePage.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import css from './TitlePage.module.css';
 
 function TitlePage({ title }) {
-  return (
-      <h1 className={css.titleContainer}>{title} </h1>
-  );
+  return <h1 className={css.titleContainer}>{title} </h1>;
 }
 
 export default TitlePage;
