0a2b9c40964f6bcd830ba8f436e850a6f32a650d
Nikita0708 <nreva97@gmail.com>
2023-11-19 23:56:52 +0100

    update styles exercises

--- src/pages/Exercises/Exercises.jsx
+++ src/pages/Exercises/Exercises.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import sprite from '../../assets/icons-optimized.svg';
+
 import css from './Exercises.module.css';
 import { ExercisesNavigation } from '../../components/Exercises/ExercisesNavigation/ExercisesNavigation';
 import { BodyPartList } from '../../components/Exercises/ExercisesSubcategoriesList/BodyPartList';
@@ -37,12 +39,16 @@ const Exercises = () => {
   return (
     <div className={css.div}>
       <div className={css.exercisesWrapper}>
-        <div className={css.exercisesBox}>
-          {activeName && (
-            <button type="button" onClick={() => setActiveName('')}>
+        {activeName && (
+            <button type="button" onClick={() => setActiveName('')} className={css.btnBack}>
+              <svg className={css.svgBack} width={16} height={16} >
+                <use href={sprite + "#icon-arrow-left"}></use>
+              </svg>
               Back
             </button>
           )}
+        <div className={css.exercisesBox}>
+          
           {activeFilter !== 'Waist' ? (
             <h2 className={css.exercisesTitle}>Exercises</h2>
           ) : (
