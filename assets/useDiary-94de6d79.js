f95f71c2f95f7c0cedf9e6eff9c23fc700442e03
OlgaGresko <122181692+OlgaGresko@users.noreply.github.com>
2023-11-19 00:39:54 +0000

    Deploying to gh-pages from @ kostarusin/power-pulse-front-end-react@85a24e059c317be9635cd0e4e8f26836720f14e0 🚀

---
+++ assets/useDiary-94de6d79.js
@@ -0,0 +1,1 @@
+import{h as s}from"./index-8aa30e45.js";const o=e=>e.diary.doneExercises,r=e=>e.diary.consumedProducts,c=e=>e.diary.burnedCalories,d=e=>e.diary.consumedCalories,t=e=>e.diary.date,a=()=>({doneExercises:s(o),consumedProducts:s(r),burnedCalories:s(c),consumedCalories:s(d),date:s(t)});export{a as u};
