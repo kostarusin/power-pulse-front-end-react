8bdf5459aa1efa2003363d11b2a4ba0f473415bb
ArtemBortyaniy <117488314+ArtemBortyaniy@users.noreply.github.com>
2023-11-20 19:02:15 +0000

    Deploying to gh-pages from @ kostarusin/power-pulse-front-end-react@5415636474da728254b45f4a791574d4abdd9565 🚀

---
+++ assets/useDiary-4465ed08.js
@@ -0,0 +1,1 @@
+import{i as s}from"./index-e4b42d88.js";const o=e=>e.diary.doneExercises,r=e=>e.diary.consumedProducts,c=e=>e.diary.burnedCalories,d=e=>e.diary.consumedCalories,t=e=>e.diary.date,a=()=>({doneExercises:s(o),consumedProducts:s(r),burnedCalories:s(c),consumedCalories:s(d),date:s(t)});export{a as u};
