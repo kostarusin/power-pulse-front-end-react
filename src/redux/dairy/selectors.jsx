1a724dde191fb14a5309125be08e9c142400ba5f
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2023-11-22 11:39:49 +0100

    loader-diary

--- src/redux/dairy/selectors.jsx
+++ src/redux/dairy/selectors.jsx
@@ -3,4 +3,4 @@ export const selectConsumedProducts = (state) => state.diary.consumedProducts;
 export const selectBurnedCalories = (state) => state.diary.burnedCalories;
 export const selectConsumedCalories = (state) => state.diary.consumedCalories;
 export const selectDate = (state) => state.diary.date;
-
+export const selectIsLoading = (state) => state.diary.isLoading;
--- src/redux/dairy/slice.jsx
+++ src/redux/dairy/slice.jsx
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { getDiary, deleteExerciseOrProduct } from './operations';
 
 const initialState = {
+  isLoading: false,
   consumedProducts: [],
   doneExercises: [],
   burnedCalories: null,
@@ -18,26 +19,37 @@ const diarySlice = createSlice({
   initialState,
   extraReducers: (builder) => {
     builder
+      .addCase(getDiary.pending, (state) => {
+        state.isLoading = true;
+      })
       .addCase(getDiary.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
         state.consumedProducts = payload.consumedProducts;
         state.doneExercises = payload.doneExercises;
         state.burnedCalories = payload.burnedCalories;
         state.consumedCalories = payload.consumedCalories;
-
         state.date = payload.date;
-           })
+      })
       .addCase(getDiary.rejected, (state) => {
+        state.isLoading = false;
         state.consumedProducts = [];
         state.doneExercises = [];
         state.burnedCalories = null;
         state.consumedCalories = null;
         state.date = null;
       })
+      .addCase(deleteExerciseOrProduct.pending, (state) => {
+        state.isLoading = true;
+      })
       .addCase(deleteExerciseOrProduct.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
         state.consumedProducts = payload.consumedProducts;
         state.doneExercises = payload.doneExercises;
         state.burnedCalories = payload.burnedCalories;
         state.consumedCalories = payload.consumedCalories;
+      })
+      .addCase(deleteExerciseOrProduct.rejected, (state) => {
+        state.isLoading = false;
       });
   },
 });
