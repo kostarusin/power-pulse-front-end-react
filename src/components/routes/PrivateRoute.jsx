7ed20675113f5d4c6280049f55502ef582631d16
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-11-22 15:49:09 +0200

    Update PrivateRoute.jsx

--- src/components/routes/PrivateRoute.jsx
+++ src/components/routes/PrivateRoute.jsx
@@ -1,14 +1,28 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../hooks';
 
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ToastIconError from '../../components/toastComponents/ToastIconError';
+import CloseBtn from '../../components/toastComponents/CloseBtn';
+import css from '../../components/toastComponents/ToastIconError/ToastIconError.module.css';
+
 const PrivateRoute = ({ component: Component }) => {
   const { isLoggedIn, isRefreshing, user } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefreshing;
-  const UpdateInfoRedirect = isLoggedIn && !user.bmr;
+  const UpdateInfoRedirect = isLoggedIn && !user.height;
 
   if (shouldRedirect) {
     return <Navigate to="/" />;
   } else if (UpdateInfoRedirect) {
+    toast.info('Fill out your profile!', {
+      position: 'top-center',
+      className: css.customToastError,
+      progressClassName: css.toastProgressBar,
+      icon: ToastIconError,
+      closeButton: CloseBtn,
+      toastId: 'registerErrorToast',
+    });
     return <Navigate to="/profile" />;
   } else {
     return <Component />;
